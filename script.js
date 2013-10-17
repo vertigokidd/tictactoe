ada00c1ee143e73f46ef62766ebaa0987192c51e
Michael Rhee <mjrhee@gmail.com>
2013-10-17 15:10:03 -0500

    Get Xs and Ox to alternate

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+  var count = 0;
   $('td').click(function(){
-    $(this).html('X');
+    count += 1;
+    if(count % 2 === 0) {
+      $(this).html('X');
+    }
+    else {
+      $(this).html('O');
+    }
   })
 });
