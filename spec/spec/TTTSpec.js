bf4e1324e2c9dc67e0e5ec5011db36a2d89f4711
Michael Rhee <mjrhee@gmail.com>
2013-10-23 13:52:34 -0500

    Finish game tests

--- spec/spec/TTTSpec.js
+++ spec/spec/TTTSpec.js
@@ -72,7 +72,18 @@ describe("Game", function() {
     expect($('td').first().html()).toEqual('X');
   });
 
+  it("should keep game over status as false if board is not full", function() {
+    game.checkFullness();
+    expect(game.over).toEqual(false);
+  });
 
+  it("should change game over status to true if board is full", function() {
+    for(i=0;i<game.squares.length;i++) {
+      game.squares[i].letter = 'X';
+    }
+    game.checkFullness();
+    expect(game.over).toEqual(true);
+  });
 
 });
 
@@ -142,53 +153,7 @@ describe("Game Diag Checking", function() {
 
 });
 
+describe("Computer Logic", function() {
 
-  // it("should be able to play a Song", function() {
-  //   player.play(song);
-  //   expect(player.currentlyPlayingSong).toEqual(song);
 
-  //   //demonstrates use of custom matcher
-  //   expect(player).toBePlaying(song);
-  // });
-
-  // describe("when song has been paused", function() {
-  //   beforeEach(function() {
-  //     player.play(song);
-  //     player.pause();
-  //   });
-
-  //   it("should indicate that the song is currently paused", function() {
-  //     expect(player.isPlaying).toBeFalsy();
-
-  //     // demonstrates use of 'not' with a custom matcher
-  //     expect(player).not.toBePlaying(song);
-  //   });
-
-  //   it("should be possible to resume", function() {
-  //     player.resume();
-  //     expect(player.isPlaying).toBeTruthy();
-  //     expect(player.currentlyPlayingSong).toEqual(song);
-  //   });
-  // });
-
-  // // demonstrates use of spies to intercept and test method calls
-  // it("tells the current song if the user has made it a favorite", function() {
-  //   spyOn(song, 'persistFavoriteStatus');
-
-  //   player.play(song);
-  //   player.makeFavorite();
-
-  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  // });
-
-  // //demonstrates use of expected exceptions
-  // describe("#resume", function() {
-  //   it("should throw an exception if song is already playing", function() {
-  //     player.play(song);
-
-  //     expect(function() {
-  //       player.resume();
-  //     }).toThrow("song is already playing");
-  //   });
-  // });
-// });
+});
