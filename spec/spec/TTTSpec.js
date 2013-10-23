597d9b9f41f2ef502c533509509d38d803cc2b6d
Michael Rhee <mjrhee@gmail.com>
2013-10-23 11:48:05 -0500

    Add spy on markBoard

--- spec/spec/TTTSpec.js
+++ spec/spec/TTTSpec.js
@@ -1,7 +1,43 @@
-describe("Game", function() {
-  // var game;
+describe("Page", function() {
+  
+  it("should have variable count", function() {
+    expect(count).toBeTruthy();
+  });
 
-  it("should create a new instance on page load", function() {
+  it("should have variable game", function() {
+    expect(game).toBeTruthy();
+  });
+
+  it("should have variable computer", function() {
+    expect(computer).toBeTruthy();
+  });
+
+  it("should have function markBoard", function() {
+    expect(markBoard).toBeTruthy();
+  });
+
+});
+
+describe("Marking", function() {
+
+  it("should increment count by 1", function() {
+    markBoard($('td').first(), game);
+    expect(count).toEqual(2);
+  });
+
+  it("should call playRound on a game when called", function() {
+    spyOn(game, 'playRound');
+    markBoard($('td')[1], game);
+    expect(game.playRound).toHaveBeenCalled();
+  });
+
+});
+
+
+
+describe("Game", function() {
+
+  it("should create a new game instance on page load", function() {
     expect(game).toBeTruthy();
   });
 
@@ -13,10 +49,6 @@ describe("Game", function() {
     expect(typeof game.squares[0]).toEqual('object');
   });
 
-  it("should have function markBoard", function() {
-    expect(markBoard).toBeTruthy();
-  });
-
   it("should respond to click on td element", function() {
     $('td').first().trigger('click');
     expect(game.squares[0].letter).toEqual('X');
@@ -24,8 +56,9 @@ describe("Game", function() {
 
   it("should change letters between turns", function() {
     game.changeLetter();
-    expect(game.turn).toEqual('O');
+    expect(game.turn).toEqual('X');
   })
+
 
 });
 
